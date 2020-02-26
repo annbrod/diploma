22fb21144b6fc14f723df1cb2ae8eaab7bbae59a
Anastasia Tulasova <annbrod73@gmail.com>
2020-02-26 21:33:47 +0400

    fix editor format

--- src/about.js
+++ src/about.js
@@ -4,18 +4,18 @@ import "./pages/about.css";
 import Swiper from 'swiper';
 
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    slidesPerColumn: 0,
-    loop: true,
-    uniqueNavElements: true,
-    loopedSlides: 6,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper__button-next',
-        prevEl: '.swiper__button-prev',
-    },
+  slidesPerView: 'auto',
+  slidesPerColumn: 0,
+  loop: true,
+  uniqueNavElements: true,
+  loopedSlides: 6,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper__button-next',
+    prevEl: '.swiper__button-prev',
+  },
 });
